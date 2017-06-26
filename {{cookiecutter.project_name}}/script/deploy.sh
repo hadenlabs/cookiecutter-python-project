2bf3e8f2b28a7c039d6a68e371bb63517834dd42
@slovacus <slovacus@gmail.com>
2017-06-25 20:54:07 -0500

    !#5 Fix: clean script deploy

--- {{cookiecutter.project_name}}/script/deploy.sh
+++ {{cookiecutter.project_name}}/script/deploy.sh
@@ -3,8 +3,3 @@
 
 # shellcheck source=/dev/null
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
-
-ansible-playbook "${FILE_PLAYBOOK}" -i "${PATH_INVENTORY}"  -v \
-                 --user="${USER}"  --private-key="${PRIVATE_KEY}" \
-                 --extra-vars "${EXTRA_VARS}" \
-                 --vault-password-file ~/.vault_pass.txt
